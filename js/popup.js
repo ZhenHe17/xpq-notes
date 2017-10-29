812f518c264a34283f6a625a7f8b7ca7fa1fa5e8
ZhenHe17 <geshengming17@163.com>
2017-10-29 19:26:27 +0800

    添加删除按钮

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,7 @@
 
 var createBtn = document.querySelector('#xpqCreateNewNote');
 var backBtn = document.querySelector('#xpqBackHomeBtn');
+var deleteBtn = document.querySelector('#xpqDeleteNoteBtn');
 var homeContainer = document.querySelector('#xpqHomeContainer');
 var notesDetailContainer = document.querySelector('#xpqNotesDetailContainer');
 
@@ -10,5 +11,9 @@ createBtn.addEventListener('click',function(e){
 })
 
 backBtn.addEventListener('click',function(e){
+  homeContainer.style.marginLeft = '0px';
+})
+
+deleteBtn.addEventListener('click',function(e){
   homeContainer.style.marginLeft = '0px';
 })
